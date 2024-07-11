006a24a544ebb8ef7f37f03395e24aa1a9fb8dce
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-07-11 14:06:59 +0300

    Accept data and change rows

--- DOM Manipulation/6.update-content/script.js
+++ DOM Manipulation/6.update-content/script.js
@@ -1,3 +1,6 @@
-function changeContent() {
-    
+function changeContent(row, cell, text) {
+    const rowElement = document.querySelector(`tr:nth-of-type(${row})`);
+    const cellElement = rowElement.children[cell];
+
+    cellElement.textContent = text;
 }
