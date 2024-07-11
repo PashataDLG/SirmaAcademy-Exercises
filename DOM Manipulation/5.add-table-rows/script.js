4ac34ae0c84f3a21887bb74ea5900136c5fe5997
Angel Pashov <86370508+PashataDLG@users.noreply.github.com>
2024-07-11 13:27:26 +0300

    rows adding functionallity created

--- DOM Manipulation/5.add-table-rows/script.js
+++ DOM Manipulation/5.add-table-rows/script.js
@@ -1,3 +1,17 @@
 function insertRow() {
-    console.log('clicked');
+    const tableElement = document.querySelector('#sampleTable > tbody');
+
+    const rowCount = tableElement.children.length;
+
+    const newRow = document.createElement('tr');
+    const newTd1 = document.createElement('td');
+    const newTd2 = document.createElement('td');
+
+    newTd1.textContent = `Row${rowCount + 1} cell1`;
+    newTd2.textContent = `Row${rowCount + 1} cell2`;
+
+    newRow.appendChild(newTd1);
+    newRow.appendChild(newTd2);
+
+    tableElement.appendChild(newRow);
 }
